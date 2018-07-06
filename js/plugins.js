61663cddadf42bc1bd87dfbd8c5b320937b0327d
okayuvich <zaytsev.main@gmail.com>
2018-07-06 15:21:07 +0900

    20180706-7

--- js/plugins.js
+++ js/plugins.js
@@ -4,5 +4,5 @@ var $plugins =
 [
 {"name":"Community_Basic","status":true,"description":"基本的なパラメーターを設定するプラグインです。","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
 {"name":"MadeWithMv","status":false,"description":"メイン画面へ進む前に、\"Made with MV\"のスプラッシュ画面もしくはカスタマイズされたスプラッシュ画面を表示します。","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}},
-{"name":"OnlineAvatar","status":false,"description":"Firebaseを使ってプレイヤーをオンライン同期します。","parameters":{"apiKey":"AIzaSyBP2cwOd-zaemuteNtw3syNhGaB6tQyxWE","authDomain":"test-project-40f50.firebaseapp.com","databaseURL":"https://test-project-40f50.firebaseio.com","avatarEvent":"1","syncSwitchStart":"11","syncSwitchEnd":"20","syncVariableStart":"11","syncVariableEnd":"20"}}
+{"name":"OnlineAvatar","status":true,"description":"Firebaseを使ってプレイヤーをオンライン同期します。","parameters":{"apiKey":"AIzaSyBP2cwOd-zaemuteNtw3syNhGaB6tQyxWE","authDomain":"test-project-40f50.firebaseapp.com","databaseURL":"https://test-project-40f50.firebaseio.com","avatarEvent":"1","syncSwitchStart":"11","syncSwitchEnd":"20","syncVariableStart":"11","syncVariableEnd":"20"}}
 ];
